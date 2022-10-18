fc85f2dd833ab314bc44f93229f394ab232f2648
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-10-18 12:40:04 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course/expenses/ExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course.expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
